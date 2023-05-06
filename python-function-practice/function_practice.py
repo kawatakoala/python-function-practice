cc2fd46f9e4a46d8f08a5fb926763319893f3ab0
Michael Kawata <michaelkawata@bens-air.lan>
2023-05-05 20:59:58 -0700

    #2

--- python-function-practice/function_practice.py
+++ python-function-practice/function_practice.py
@@ -3,3 +3,14 @@ def max_num(x, y, z):
 
 
 print(max_num(5, 3, 2))
+
+
+def mult_list(myList):
+    result = 1
+    for x in myList:
+        result = result * x
+    return result
+
+
+list1 = [3, 2, 4]
+print(mult_list(list1))
